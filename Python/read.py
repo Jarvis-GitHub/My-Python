0aea3aad317fe4cd39b8d7efba0b6c55baf012d6
Jarvis-GitHub <love642108192@gmail.com>
2018-09-11 21:13:30 +0800

    123

--- Python/read.py
+++ Python/read.py
@@ -1,5 +1,7 @@
-file1 = open(input('>'),encoding='UTF-8')
+file1 = open('c:/Users/Jarvis/Desktop/a.txt', encoding='UTF-8')
 a = file1.readlines()
-
+file2 = open('c:/Users/Jarvis/Desktop/b.txt', mode='r+', encoding='UTF-8')
+b = file1.readlines()
 for i in a:
-    print(i)
+    j = i.split()
+    file2.writelines(f"{j[1]} {j[0]}\n")
